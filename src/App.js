288137a3f009b1e7f48965929c8e23e58f2ec916
Edgardo Ezequiel Sarmiento <129633447+EzeSar@users.noreply.github.com>
2023-09-22 00:48:47 -0300

    flex wrap, flex grow

--- src/App.js
+++ src/App.js
@@ -6,18 +6,18 @@ function App() {
       <div className='div-clima'>
         <div className='div-temperatura'>
           <div className='div-temp-actual'>
-            TEMP.ACTUAL:20°
+            TEMP. ACTUAL: 20°
           </div>
           <div className='div-fecha-hora'>
             LUNES 10/05/23 11:36 A.M.
           </div>
           <div className='div-max-min'>
-            MIN:12°/MAX:25°
+            MIN:12° / MAX:25°
           </div>
         </div>
         <div className='div-datos-clima'>
           <div className='div-temp-x-hora'>
-            GRAFICO TEMP.A CADA HORA
+            GRAFICO TEMP. A CADA HORA
           </div>
           <div className='div-otros-datos'>
             DATOS COMPLEMENTARIOS

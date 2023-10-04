0a27b5cb434b0cb88a5ebc9be2f352c1942639b3
Edgardo Ezequiel Sarmiento <129633447+EzeSar@users.noreply.github.com>
2023-10-04 00:18:43 -0300

    weathercodes v2 sin implementar

--- src/components/WeatherCodes.jsx
+++ src/components/WeatherCodes.jsx
@@ -1,4 +1,52 @@
-export const WeaterCodes = {
+import despejado_dia from '../assets/animation-weather/despejado_dia.svg';
+import despejado_noche from '../assets/animation-weather/despejado_noche.svg';
+import parcial_nublado_dia from '../assets/animation-weather/parcial_nublado_dia.svg';
+import parcial_nublado_noche from '../assets/animation-weather/parcial_nublado_noche.svg';
+import nublado from '../assets/animation-weather/nublado.svg';
+import niebla_dia from '../assets/animation-weather/niebla_dia.svg';
+import niebla_noche from '../assets/animation-weather/niebla_noche.svg';
+import llovizna_ligera_dia from '../assets/animation-weather/llovizna_ligera_dia.svg';
+import llovizna_ligera_noche from '../assets/animation-weather/llovizna_ligera_noche.svg';
+import llovizna_moderada_dia from '../assets/animation-weather/llovizna_moderada_dia.svg';
+import llovizna_moderada_noche from '../assets/animation-weather/llovizna_moderada_noche.svg';
+import llovizna_densa_dia from '../assets/animation-weather/llovizna_densa_dia.svg';
+import llovizna_densa_noche from '../assets/animation-weather/llovizna_densa_noche.svg';
+import llovizna_helada_ligera_dia from '../assets/animation-weather/llovizna_helada_ligera_dia.svg';
+import llovizna_helada_ligera_noche from '../assets/animation-weather/llovizna_helada_ligera_noche.svg';
+import llovizna_helada_densa_dia from '../assets/animation-weather/llovizna_helada_densa_dia.svg';
+import llovizna_helada_densa_noche from '../assets/animation-weather/llovizna_helada_densa_noche.svg';
+import lluvia_leve_dia from '../assets/animation-weather/lluvia_leve_dia.svg';
+import lluvia_leve_noche from '../assets/animation-weather/lluvia_leve_noche.svg';
+import lluvia_moderada_dia from '../assets/animation-weather/lluvia_moderada_dia.svg';
+import lluvia_moderada_noche from '../assets/animation-weather/lluvia_moderada_noche.svg';
+import lluvia_fuerte_dia from '../assets/animation-weather/lluvia_fuerte_dia.svg';
+import lluvia_fuerte_noche from '../assets/animation-weather/lluvia_fuerte_noche.svg';
+import nieve_leve_dia from '../assets/animation-weather/nieve_leve_dia.svg';
+import nieve_leve_noche from '../assets/animation-weather/nieve_leve_noche.svg';
+import nieve_moderada_dia from '../assets/animation-weather/nieve_moderada_dia.svg';
+import nieve_moderada_noche from '../assets/animation-weather/nieve_moderada_noche.svg';
+import nieve_fuerte_dia from '../assets/animation-weather/nieve_fuerte_dia.svg';
+import nieve_fuerte_noche from '../assets/animation-weather/nieve_fuerte_noche.svg';
+import granizo_dia from '../assets/animation-weather/granizo_dia.svg';
+import granizo_noche from '../assets/animation-weather/granizo_noche.svg';
+import chubascos_leves_dia from '../assets/animation-weather/chubascos_leves_dia.svg';
+import chubascos_leves_noche from '../assets/animation-weather/chubascos_leves_noche.svg';
+import chubascos_moderados_dia from '../assets/animation-weather/chubascos_moderados_dia.svg';
+import chubascos_moderados_noche from '../assets/animation-weather/chubascos_moderados_noche.svg';
+import chubascos_violentos_dia from '../assets/animation-weather/chubascos_violentos_dia.svg';
+import chubascos_violentos_noche from '../assets/animation-weather/chubascos_violentos_noche.svg';
+import chubascos_nieve_ligeros_dia from '../assets/animation-weather/chubascos_nieve_ligeros_dia.svg';
+import chubascos_nieve_ligeros_noche from '../assets/animation-weather/chubascos_nieve_ligeros_noche.svg';
+import chubascos_nieve_fuertes_dia from '../assets/animation-weather/chubascos_nieve_fuertes_dia.svg';
+import chubascos_nieve_fuertes_noche from '../assets/animation-weather/chubascos_nieve_fuertes_noche.svg';
+import tormenta_dia from '../assets/animation-weather/tormenta_dia.svg';
+import tormenta_noche from '../assets/animation-weather/tormenta_noche.svg';
+import tormenta_granizo_leve_dia from '../assets/animation-weather/tormenta_granizo_leve_dia.svg';
+import tormenta_granizo_leve_noche from '../assets/animation-weather/tormenta_granizo_leve_noche.svg';
+import tormenta_granizo_fuerte_dia from '../assets/animation-weather/tormenta_granizo_fuerte_dia.svg';
+import tormenta_granizo_fuerte_noche from '../assets/animation-weather/tormenta_granizo_fuerte_noche.svg';
+
+const WeaterCodes = {
   0: 'despejado',
   1: 'mayormente despejado', 
   2: 'parcialmente nublado', 

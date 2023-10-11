a5a70a7fb51e05d817f05c2f7b962ef698f127d4
Edgardo Ezequiel Sarmiento <129633447+EzeSar@users.noreply.github.com>
2023-10-11 06:25:36 -0300

    fetch con await y corrijo hora_sola

--- src/App.js
+++ src/App.js
@@ -5,17 +5,18 @@ import { Datos } from "./components/Datos";
 
 function App() {
 
-  let [datosApi, setDatosApi] = useState();
+  let [datosApi, setDatosApi] = useState(Datos);
 
   useEffect(()=>{
 
-    fetch("https://api.open-meteo.com/v1/forecast?latitude=-31.4135&longitude=-64.181&hourly=temperature_2m,relativehumidity_2m,precipitation_probability,visibility,windspeed_10m&daily=weathercode,temperature_2m_max,temperature_2m_min,sunrise,sunset,uv_index_max&current_weather=true&timezone=America%2FSao_Paulo&forecast_days=1")
-    .then(resp => resp.json())
-    .then(data => {
-      setDatosApi(data);
-    }).catch(ex => {
-      console.error(ex);
-    })
+    async function obtenerDatosDeApi(){
+      let respuesta = await fetch("https://api.open-meteo.com/v1/forecast?latitude=-31.4135&longitude=-64.181&hourly=temperature_2m,relativehumidity_2m,precipitation_probability,visibility,windspeed_10m&daily=weathercode,temperature_2m_max,temperature_2m_min,sunrise,sunset,uv_index_max&current_weather=true&timezone=America%2FSao_Paulo&forecast_days=1");
+      let datos = await respuesta.json();
+      setDatosApi(datos);
+      console.log(datosApi);
+    }
+
+    obtenerDatosDeApi();
 
   },[]);
 

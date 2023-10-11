efb1f7b9a4abd2ad6f3b4e1d053c7a1545d4011d
Edgardo Ezequiel Sarmiento <129633447+EzeSar@users.noreply.github.com>
2023-10-10 21:32:09 -0300

    agrego fetch con .then

--- src/App.js
+++ src/App.js
@@ -5,15 +5,25 @@ import { Datos } from "./components/Datos";
 
 function App() {
 
-  const [datosApi2, setDatosApi2] = useState(Datos);
+  let [datosApi, setDatosApi] = useState();
 
-  useEffect(()=>{setDatosApi2(Datos)},[]);
+  useEffect(()=>{
+
+    fetch("https://api.open-meteo.com/v1/forecast?latitude=-31.4135&longitude=-64.181&hourly=temperature_2m,relativehumidity_2m,precipitation_probability,visibility,windspeed_10m&daily=weathercode,temperature_2m_max,temperature_2m_min,sunrise,sunset,uv_index_max&current_weather=true&timezone=America%2FSao_Paulo&forecast_days=1")
+    .then(resp => resp.json())
+    .then(data => {
+      setDatosApi(data);
+    }).catch(ex => {
+      console.error(ex);
+    })
+
+  },[]);
 
   return (
 
     <div className="App">
 
-      <Clima datos={datosApi2} />
+      <Clima datos={datosApi} />
 
       <Transporte />
       
